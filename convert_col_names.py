3de0c5b2e927b8322a2d36a774281fb6c720420f
James Zhang <rz234@cornell.edu>
2024-03-25 22:10:08 -0400

    Update script to convert column names

--- convert_col_names.py
+++ convert_col_names.py
@@ -1,7 +1,16 @@
 with open("data/quarterly_var_names.csv", "r") as file:
     lines = [line.rstrip() for line in file]
     lines = lines[1:]
-    converted = ["gvkey VARCHAR(256)", "datadate DATE"]
+    print(len(lines))
+    converted = ["gvkey VARCHAR(256)",
+                 "datadate DATE",
+                 "indfmt VARCHAR(256)",
+                 "consol VARCHAR(256)",
+                 "popsrc VARCHAR(256)",
+                 "datafmt VARCHAR(256)",
+                 "curcdq VARCHAR(256)",
+                 "costat VARCHAR(256)"]
+    names = []
     for line in lines:
         line_arr = line.split(",")
         # if len(line_arr) != 3:
@@ -17,6 +26,20 @@ with open("data/quarterly_var_names.csv", "r") as file:
         else:
             print(type)
             assert False
-        converted.append(name + " " + converted_type + ",")
+        names.append(name)
+        converted.append(name + " " + converted_type)
+        converted = list(set(converted))
     with open("converted.txt", "w") as output:
-        output.write("\n".join(converted))
+        output.write(",\n".join(converted))
+
+with open("data/quarterly_fundamentals.csv", "r") as file:
+    lines = [line.rstrip() for line in file]
+    real_names = lines[0].split(",")
+    print(real_names)
+    for n in real_names:
+        if n not in names:
+            print(n)
+    print("-" * 50)
+    for n in names:
+        if n not in real_names:
+            print(n)
